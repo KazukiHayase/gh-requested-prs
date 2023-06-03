df3c2d8e22e60510825f0d6da56b7f51bfb6fb94
KazukiHayase <k.hayase@buysell-technologies.com>
2023-06-03 20:02:33 +0900

    fetch prs and open in browser

--- go.mod
+++ go.mod
@@ -2,11 +2,13 @@ module github.com/KazukiHayase/gh-open-prs
 
 go 1.20
 
-require github.com/cli/go-gh/v2 v2.0.0
+require (
+	github.com/cli/go-gh/v2 v2.0.0
+	github.com/cli/shurcooL-graphql v0.0.3
+)
 
 require (
 	github.com/cli/safeexec v1.0.0 // indirect
-	github.com/cli/shurcooL-graphql v0.0.3 // indirect
 	github.com/henvic/httpretty v0.0.6 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
--- main.go
+++ main.go
@@ -2,25 +2,79 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os/exec"
+	"strings"
 
+	"github.com/cli/go-gh/v2"
 	"github.com/cli/go-gh/v2/pkg/api"
+	graphql "github.com/cli/shurcooL-graphql"
 )
 
 func main() {
-	fmt.Println("hi world, this is the gh-open-prs extension!")
-	client, err := api.DefaultRESTClient()
+	client, err := api.DefaultGraphQLClient()
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
-	response := struct {Login string}{}
-	err = client.Get("user", &response)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("running as %s\n", response.Login)
-}
 
-// For more examples of using go-gh, see:
-// https://github.com/cli/go-gh/blob/trunk/example_gh_test.go
+	var query struct {
+		Search struct {
+			Edges []struct {
+				Node struct {
+					PullRequest struct {
+						URL string
+					} `graphql:"... on PullRequest"`
+				}
+			}
+		} `graphql:"search(first: 100, query: $q, type: ISSUE)"`
+	}
+
+	orgList, _, err := gh.Exec("org", "list")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	orgs := strings.Split(orgList.String(), "\n")
+
+	filters := []string{
+		"is:pr",
+		"is:open",
+		"review-requested:@me",
+	}
+
+	for _, org := range orgs {
+		fmt.Println(org)
+		if org == "" {
+			continue
+		}
+
+		filters = append(filters, fmt.Sprintf("org:%s", org))
+	}
+
+	variables := map[string]any{
+		"q": graphql.String(strings.Join(filters, " ")),
+	}
+
+	if err := client.Query("ReviewRequestedPullRequests", &query, variables); err != nil {
+		log.Fatal(err)
+	}
+
+	var urls []string
+	for _, edge := range query.Search.Edges {
+		urls = append(urls, edge.Node.PullRequest.URL)
+	}
+
+	if len(urls) == 0 {
+		fmt.Println("no review requested pull requests")
+		return
+	}
+
+	fmt.Println("open pull requests...")
+	for _, url := range urls {
+		fmt.Println(url)
+		err := exec.Command("open", url).Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
